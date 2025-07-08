c0d5938036886f232fd54651046f781c28b00e44
David Simmons <dr.desimmons@gmail.com>
2025-07-08 20:58:21 +0100

    dart:ui -> dart:ui_web

--- lib/swagger_documentation_widget.dart
+++ lib/swagger_documentation_widget.dart
@@ -1,8 +1,9 @@
 library swagger_documentation_widget;
 
 import 'dart:html' as html;
-import 'dart:ui' as ui;
+import 'dart:ui_web' as ui;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 /// Controls the default expansion setting for the operations and tags.
@@ -146,7 +147,6 @@ class _SwaggerDocumentationState extends State<SwaggerDocumentationWidget> {
 
     // Make sure to call registerViewFactory only once
     if (!viewTypeIsRegistered) {
-      // ignore: undefined_prefixed_name
       ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) {
         final iframe = html.IFrameElement()
           ..src =
